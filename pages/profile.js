41610498ffbbc67dcd2d2b37e5b005be81df04dd
Storm <yannic.freson1@gmail.com>
2023-03-03 17:35:12 +0100

    Update profile.js

--- pages/profile.js
+++ pages/profile.js
@@ -7,11 +7,11 @@ export default function Home() {
     <div>
       <div className="absolute top-o left-0 w-full px-4">
         <TopBar />
-        <div className="w-full bg-slate-200 h-[0.1rem] mt-4"></div>
+        <div className="w-full bg-slate-200 h-[0.1rem] mt-4 rounded-sm"></div>
       </div>
 
       <div className="absolute bottom-0 left-0 w-full mb-4 px-4">
-        <div className="w-full bg-slate-200 h-[0.1rem] mb-4"></div>
+        <div className="w-full bg-slate-200 h-[0.1rem] mb-4 rounded-sm"></div>
         <Nav />
       </div>
     </div>
